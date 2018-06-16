7c73b64787f5121f1fed60b31cd8c4402cb22ffa
Ruben Sanchez <SanchezVRuben@gmail.com>
2018-06-16 15:45:12 -0700

    started invertManager

---
+++ InverseWorld_Project/Assets/_Scripts/InvertManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 
+/// 
+/// Ruben Sanchez
+/// 
+/// </summary>
+
+public class InvertManager : MonoBehaviour
+{
+    public static InvertManager Instance;
+
+    public delegate void Invert(bool isInverted);
+    public event Invert OnInvert;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+
+            DontDestroyOnLoad(gameObject);
+        }
+
+        else
+            Destroy(gameObject);
+    }
+
+    public void InvertState(bool isInverted)
+    {
+        if(OnInvert != null)
+            OnInvert.Invoke(isInverted);
+    }
+}
