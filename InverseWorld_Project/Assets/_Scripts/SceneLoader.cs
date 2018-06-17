daa4ad2537ffc421571210c3bcd4bc1f92314ffc
Michaelwolf95 <michaelwolf95@gmail.com>
2018-06-17 10:36:38 -0700

    Added Menus

---
+++ InverseWorld_Project/Assets/_Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace VGDA.UI
+{
+    public class SceneLoader :MonoBehaviour
+    {
+        public int SceneIndex = 0;
+        public void LoadScene()
+        {
+            SceneManager.LoadScene(SceneIndex);
+        }
+        public void LoadScene(float delay)
+        {
+            Invoke("LoadScene", delay);//SceneManager.LoadScene(SceneIndex));
+        }
+    }
+}
