00e23f18c37a9b5d0b41f9c313117d87ed45a748
Michaelwolf95 <michaelwolf95@gmail.com>
2018-06-16 21:14:28 -0700

    Particles

---
+++ InverseWorld_Project/Assets/_Scripts/InverseWorld/InvertParticleColor.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace VGDA.InverseWorld
+{
+    public class InvertParticleColor : InvertSubscriber
+    {
+        public ParticleSystem pSystem;
+        public Color normalColor = Color.white;
+        public Color invertColor = Color.black;
+
+        protected override void Start()
+        {
+            base.Start();
+            if (!pSystem)
+            {
+                pSystem = GetComponent<ParticleSystem>();
+            }
+            DoInvert(false);
+        }
+
+        protected override void DoInvert(bool isInverted)
+        {
+            Color newColor = (isInverted) ? invertColor : normalColor;
+            var main = pSystem.main;
+            main.startColor = newColor;
+
+
+            ParticleSystem.Particle[] particles = new ParticleSystem.Particle[pSystem.particleCount];
+            pSystem.GetParticles(particles);
+            foreach (ParticleSystem.Particle p in particles)
+            {
+                var particle = p;
+                particle.startColor = newColor;
+            }
+            pSystem.SetParticles(particles, pSystem.particleCount);
+
+        }
+    }
+}
