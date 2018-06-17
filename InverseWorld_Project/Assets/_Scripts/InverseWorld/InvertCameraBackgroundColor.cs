3e14d9eeed11c2fcbc8b2e85621c5e38bea3311d
Michaelwolf95 <michaelwolf95@gmail.com>
2018-06-16 19:16:31 -0700

    Shaders!

---
+++ InverseWorld_Project/Assets/_Scripts/InverseWorld/InvertCameraBackgroundColor.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace VGDA.InverseWorld
+{
+    public class InvertCameraBackgroundColor : InvertSubscriber
+    {
+        public Camera camera;
+        public Color normalColor = Color.white;
+        public Color invertColor = Color.black;
+
+        protected override void Start()
+        {
+            base.Start();
+            if (!camera)
+            {
+                camera = Camera.main;
+            }
+            DoInvert(false);
+        }
+
+        protected override void DoInvert(bool isInverted)
+        {
+            if (isInverted)
+            {
+                camera.backgroundColor = invertColor;
+            }
+            else
+            {
+                camera.backgroundColor = normalColor;
+            }
+        }
+    }
+}
