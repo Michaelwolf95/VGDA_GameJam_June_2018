2c5a3c541d41fc275c61973a0fb33471f0c622a9
Michaelwolf95 <michaelwolf95@gmail.com>
2018-06-16 17:56:14 -0700

    Invert Audio Hook Up

---
+++ InverseWorld_Project/Assets/_Scripts/InverseWorld/InvertAudioController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace VGDA.InverseWorld
+{
+    public class InvertAudioController : InvertSubscriber
+    {
+        [FMODUnity.EventRef]
+        public string MusicEventName = "event:/EVENT";
+        public string invertParameterName = "";
+
+        public FMOD.Studio.EventInstance musicEvent;
+        public FMOD.Studio.ParameterInstance invertParameter;
+
+        protected override void Start()
+        {
+            base.Start();
+            musicEvent = FMODUnity.RuntimeManager.CreateInstance(MusicEventName);
+            musicEvent.getParameter(invertParameterName, out invertParameter);
+            musicEvent.start();
+        }
+        protected override void DoInvert(bool isInverted)
+        {
+            if (isInverted)
+            {
+                invertParameter.setValue(1f);
+            }
+            else
+            {
+                invertParameter.setValue(0f);
+            }
+            
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyUp(KeyCode.BackQuote))
+            {
+                InvertManager.Instance.ToggleInvert();
+            }
+        }
+    }
+}
--- InverseWorld_Project/Assets/_Scripts/InverseWorld/InvertManager.cs
+++ InverseWorld_Project/Assets/_Scripts/InverseWorld/InvertManager.cs
@@ -14,22 +14,38 @@ public class InvertManager : MonoBehaviour
     public delegate void Invert(bool isInverted);
     public event Invert OnInvert;
 
+    public bool IsInverted { get; private set; }
+
     private void Awake()
     {
         if (Instance == null)
         {
             Instance = this;
-
+            IsInverted = false;
             DontDestroyOnLoad(gameObject);
         }
-
         else
+        {
             Destroy(gameObject);
+        }
     }
 
-    public void InvertState(bool isInverted)
+    public void ToggleInvert()
     {
-        if(OnInvert != null)
-            OnInvert.Invoke(isInverted);
+        InvertState(!IsInverted);
+    }
+
+    public void InvertState(bool argIsInverted)
+    {
+        if (IsInverted == argIsInverted)
+        {
+            return;
+        }
+
+        IsInverted = argIsInverted;
+        if (OnInvert != null)
+        {
+            OnInvert.Invoke(IsInverted);
+        }
     }
 }
