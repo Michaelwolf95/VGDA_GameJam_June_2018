b67db81f3d4855e6b9102da18b27ef6146ec15f2
Michaelwolf95 <michaelwolf95@gmail.com>
2018-06-16 20:31:23 -0700

    Basic character setup

---
+++ InverseWorld_Project/Assets/_Scripts/InverseWorld/InvertMaterialSwap.cs
@@ -0,0 +1,34 @@
+﻿
+using UnityEngine;
+
+namespace VGDA.InverseWorld
+{
+    public class InvertMaterialSwap : InvertSubscriber
+    {
+        public Renderer renderer;
+        public Material normalMaterial;
+        public Material invertedMaterial;
+
+        protected override void Start()
+        {
+            base.Start();
+            if (!renderer)
+            {
+                renderer = GetComponent<Renderer>();
+            }
+            DoInvert(false);
+        }
+
+        protected override void DoInvert(bool isInverted)
+        {
+            if (isInverted)
+            {
+                renderer.material = invertedMaterial;
+            }
+            else
+            {
+                renderer.material = normalMaterial;
+            }
+        }
+    }
+}
